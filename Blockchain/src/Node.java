06d833b72d605667970cb25599924ee7fba18015
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2018-07-10 11:27:27 +0100

    block processing

--- Blockchain/src/Node.java
+++ Blockchain/src/Node.java
@@ -1,29 +1,38 @@
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 
 public class Node {
 	private ArrayList<Block> blockchain = new ArrayList<Block>(); 
 	private int difficulty = 5;
+	private PriorityQueue<String> transactionPool;
+	private int count=0;
 	
-	public Node(ArrayList<Block> blockchain, int difficulty) {
-		this.blockchain=blockchain;
-		this.difficulty=difficulty;
+	public Node() {
+		transactionPool = new PriorityQueue<String>();
+		
 	}
+	
 	
 	public void processBlocks() {
-		blockchain.add(new Block("Hi im the first block", "0"));
-		System.out.println("Trying to mine Block 1");
-		blockchain.get(0).mineBlock(difficulty);
+	while(!transactionPool.isEmpty()) {
+		String transaction = this.getTransactionFromPool();
 		
-		blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).getHash())); 
-		System.out.println("Trying to mine Block 2");
-		blockchain.get(1).mineBlock(difficulty);
-		
-		blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).getHash()));
-		System.out.println("Trying to mine Block 3");
-		blockchain.get(2).mineBlock(difficulty);
-		
+		blockchain.add(new Block(transaction,getHash()));
+		System.out.println("Trying to mine Block" +count);
+		blockchain.get(count).mineBlock(difficulty);
+
+		count+=1;
 	}
+}
+
+	private String getHash() {
+	String hash = "0";
+	if(!(blockchain.size()== 0)) {
+		hash = blockchain.get(blockchain.size()-1).getHash();
+	}
+	return hash;
+}
 
 	//getters
 	public ArrayList<Block> getBlockchain(){
@@ -34,6 +43,22 @@ public class Node {
 	public int getDifficulty() {
 		return this.difficulty;
 	}
+	
+	public String getTransactionFromPool(){
+		String transaction = transactionPool.remove();
+		return transaction;
+		
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
+	//setters
+		public void addTransactionToPool(String transaction) {
+			transactionPool.add(transaction);
+		}
+		
 	
 	
 	
--- Blockchain/src/Server.java
+++ Blockchain/src/Server.java
@@ -5,9 +5,15 @@ import com.google.gson.GsonBuilder;
 public class Server {
 
 	public static void main(String[] args) {
-		ArrayList<Block> blockchain = new ArrayList<Block>(); 
-		int difficulty = 5;
-		Node node = new Node(blockchain, difficulty);
+
+		Node node = new Node();
+		ArrayList<String> blockData = new ArrayList<String>();
+		blockData.add("Hi Im' the first block");
+		blockData.add("Hey Im' the second block");
+		blockData.add("Yo Im' the third block");
+		for(String data: blockData) {
+			node.addTransactionToPool(data);
+		}
 		node.processBlocks();
 		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(node.getBlockchain());		
 		System.out.println("\nThe block chain: ");
